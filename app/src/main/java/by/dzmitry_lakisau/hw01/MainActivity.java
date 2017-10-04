d986d67ef32bde4d7ce52e8db419f791b4c3b27b
DzmitryLakisau <laki_dimon@mail.ru>
2017-10-04 23:42:31 +0300

    Added textview and one redundant declaration for merge conflict

--- app/src/main/java/by/dzmitry_lakisau/hw01/MainActivity.java
+++ app/src/main/java/by/dzmitry_lakisau/hw01/MainActivity.java
@@ -2,12 +2,18 @@ package by.dzmitry_lakisau.hw01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String HELLO_WORLD = "Hello World!";//this import is gonna be trashed when merging;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(HELLO_WORLD);
     }
 }
