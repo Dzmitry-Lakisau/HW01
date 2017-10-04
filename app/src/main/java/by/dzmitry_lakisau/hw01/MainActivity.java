1cb80505fcfdd93b2e928623e7eca0b573abae55
DzmitryLakisau <laki_dimon@mail.ru>
2017-10-04 22:22:51 +0300

    Added toast

--- app/src/main/java/by/dzmitry_lakisau/hw01/MainActivity.java
+++ app/src/main/java/by/dzmitry_lakisau/hw01/MainActivity.java
@@ -2,12 +2,18 @@ package by.dzmitry_lakisau.hw01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String HELLO_WORLD = "Hello world!";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast toast = Toast.makeText(getApplicationContext(), HELLO_WORLD, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
